94c871f908350693905fe91dc4decff8a54ce0f3
nebula <jing.me@icloud.com>
2026-06-03 14:19:13 +0800

    fix: 移除 tauri-plugin-log 与 tracing 冲突

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -28,7 +28,6 @@ pub fn run() {
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             commands::greet,
